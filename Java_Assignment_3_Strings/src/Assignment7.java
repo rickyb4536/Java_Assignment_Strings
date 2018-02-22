aa2a0e1340389ab4e911d6fdce2c860f010b9014
Ricky Brown <rickyb4536@gmail.com>
2018-02-22 12:00:49 -0500

    fifth commit
    Just added an alternative way to find print out only the token starting
    after the "=" symbol. I commented this section out.

--- Java_Assignment_3_Strings/src/Assignment7.java
+++ Java_Assignment_3_Strings/src/Assignment7.java
@@ -7,6 +7,22 @@ public class Assignment7 {
 		
 		//will print out only the token part after the "=" symbol
 		System.out.println(url.substring(url.length()-32));
+		
+		
+		
+		/*
+		 
+public class Assignment7 {
+
+	public static void main(String[] args) {
+		String str = "http://example.com?access_token=EFEc8328h29jndjd02h12$3829&ds98d";
+		
+		String token = str.substring(str.indexOf("=") + 1);
+		
+		System.out.println(token);
+	}
+}
+		 */
 	}
 
 }
